e340dbf0847ea2dc9e1ca3cb82dbea964d068af8
swapnika921-png <swapnika921@gmail.com>
2025-11-04 10:53:09 +0530

    Add files via upload

---
+++ smallest or largest.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int n,a[10],i,min,max;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	scanf("%d",&a[i]);
+	min=max=a[0];
+	for(i=0;i<n;i++)
+	{
+		if(a[i]>max)
+		max=a[i];
+		if(a[i]<min)
+		min=a[i];
+	}
+	printf("Max=%d\nMin=%d",max,min);
+}
